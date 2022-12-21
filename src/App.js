8888b912124e40855ccaa2e6750289d90ffaf711
tristanbagnulo <tristan.bagnulo1@gmail.com>
2022-12-22 10:53:58 +1100

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from "./Counter"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Testing counters</h2>
+        <Counter />
+      </header>
+    </div>
+  );
+}
+
+export default App;
