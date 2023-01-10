8c06fb494030bc7f662fdddbc46898c5df086419
tristanbagnulo <tristan.bagnulo1@gmail.com>
2023-01-10 17:08:25 +1100

    Finished lesson 8 & added code comments.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,59 @@
 import './App.css';
+import React, { useState, useEffect } from "react";
+import Counter from "./Components/Counter";
 
 function App() {
+  
+  // TOPIC #1 - |The React Lifecycle of a Functional Component|
+
+  // 1. |Mounting| - The component is rendered onto the 
+  // page for the first time (mounted).
+  // 2. |Updating| - Re-renders the given component when 
+  // is changes or if the component has a state variable 
+  // that updates.
+  // 3. |Unmounting| - A component is removed an is no
+  // longer shown on a page.
+
+  // e.g. - |Show and Hide Counter|
+
+  // ||Documentation||
+  // |Workflow Description|
+  // 1. When the site is loaded, only the "Show Counter"
+  // button is shown. At this point, it is not mounted.
+  // 2. When "Show Counter" is clicked, the counter 
+  // component is "Mounted" and now displays with a 
+  // current count of 0.
+  // 3. Pressing the "Increase Count" button will 
+  // "Update" the Counter component to increase its the 
+  // displayed "Current count: ". 
+  // Also when the counter is shown, the button's text
+  // updates to "Hide Counter".
+  // 4. Finally, the "Hide Coutner" button can be pressed
+  // again to hide "Unmount:" the Counter component. It
+  // it now no longer appears on the page.
+
+  // |Code Documentation|
+  /* The below code has a state variable called `showCounter` that 
+  is initialized   using the `useState` hook. The initial value of 
+  `showCounter` is false.
+  The component renders a `div` element with a heading and a button. 
+  When the button is clicked, the value of `showCounter` is toggled 
+  by calling `setShowCounter` with the negated value of `showCounter`. 
+  The text displayed on the button also changes based on the value 
+  of `showCounter`.
+  If `showCounter` is `true`, the component also renders a `Counter` 
+  component. If `showCounter` is `false`, the `Counter` component is not 
+  rendered. */
+
+  const [showCounter, setShowCounter] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-
-        {/* TODO - WHEN NEW BRANCH CREATED Remove below 3 headers. */}
-        <h1>Main Branch</h1>
-        <h2><u>Make new branches from this branch</u></h2>
-        <h2>No Functionality - Baseline project only</h2>
-
-      </header>
+        <h1>Lesson #X</h1>
+        <button onClick={() => setShowCounter(!showCounter)}>
+          {showCounter ? "Hide Counter" : "Show Counter"}
+        </button>
+        {showCounter && <Counter />}
     </div>
   );
 }
---
+++ src/Components/Counter.js
@@ -0,0 +1,27 @@
+import React, {useState, useEffect } from "react";
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    // TOPIC #2 - |The `useEffect` hook|
+    // The `useEffect` hook will run once when
+    // a component is Mounted or Updated below. More on next lesson.
+    useEffect(() => {
+        console.log("The component was mounted/updated and the useEffect ran. Run #: " + count);
+        //Adding a return makes something happen when the component
+        //is unmounted.
+        return () => {
+            console.log("The component was unmounted.");
+        }
+    });
+
+    return (
+        <div>
+            <h4>Counter</h4>
+            <p>Current count: {count}</p>
+            <button onClick={() => setCount(count +1)}>Increase Count</button>
+        </div>
+    );
+};
+
+export default Counter;
