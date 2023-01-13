0722b1cb9b2709894c4fcd3cb93a8de0ad5895d4
tristanbagnulo <tristan.bagnulo1@gmail.com>
2023-01-13 14:55:24 +1100

    Added React import to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
 
-        {/* TODO - WHEN NEW BRANCH CREATED Remove below 3 headers. */}
-        <h1>Main Branch</h1>
-        <h2><u>Make new branches from this branch</u></h2>
-        <h2>No Functionality - Baseline project only</h2>
+        
+        <h1>Lesson 10</h1>
+        
 
       </header>
     </div>
