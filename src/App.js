9af3a6f1e00ecb95475c63ecf574948323ebc235
tristanbagnulo <tristan.bagnulo1@gmail.com>
2023-01-16 16:45:26 +1100

    Finished lesson 10. Used different tutorial.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useNavigate, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from './Components/Pages/Home';
 import About from './Components/Pages/About';
 import ErrorPage from './Components/Pages/ErrorPage';
-import Profile from './Components/Pages/Profile'
-import RouterComponent from './Components/RouterComponent'
+import Profile from './Components/Pages/Profile';
+import RouterComponent from './Components/RouterComponent';
 
+  // This Router branch is based on this tutorial which
+  // is not on the main React tutorial playlist. This is because
+  // this tutorial uses React Router V6.
+  // Link - https://www.youtube.com/watch?v=UjHT_NKR_gU
 
 function App() {
 
+  // I have embeded `RouterComponent` below within 
+  // `Router` aka `BrowserRouter` because React Router, 
+  // requires that the `useNavigate` hook be used within
+  // a `BrowserRouter`. By first rendering a `BrowserRouter`
+  // and then a separate custom `RouterComponent` which
+  // I programmed and which contained the `useNavigate` hook
+  // I satisfy that requirement. 
+
+  // See a StackOverflow discussion about that...
+  // https://stackoverflow.com/questions/70491774/usenavigate-may-be-used-only-in-the-context-of-a-router-component
   return (
     <Router>
+      {/* ^^ Highest level components are `BrowserRouter` 
+      called `Router` here. */}
       <RouterComponent/>
+      <nav>
+            {/* The below 3 are Link implementations are are 
+            very different from the `useNavigator` ones used in the
+            button examples in RouterComponent.js */}
+            <Link to="/"> Home </Link> 
+            <a>|</a>
+            <Link to="/about"> About </Link> 
+            <a>|</a><br></br>
+            <Link to="/profile"> Profile </Link><a> -- requires "/username"</a>
+          </nav>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            {/* This route accepts an additional parameter
+            `username` */}
+            <Route path="/profile/:username" element={<Profile/>}/>
+            {/* The last route should be the error page. */}
+            <Route path="*" element={<ErrorPage/>}/>
+          </Routes>
     </Router>
   );
 }
--- src/Components/RouterComponent.js
+++ src/Components/RouterComponent.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useNavigate, RouterProvider } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from './Pages/Home';
 import About from './Pages/About';
 import ErrorPage from './Pages/ErrorPage';
 import Profile from './Pages/Profile';
 
-// This Router branch is based on this tutorial which
-// is not on the main React tutorial playlist. This is because
-// this tutorial uses React Router V6.
-// Link - https://www.youtube.com/watch?v=UjHT_NKR_gU
-
 function RouterComponent() {
+    // `useNavigate` can only appear in a component that is a child
+    // component of a `BrowserRouter` element. So, it's all here.
+    // 
     let navigate = useNavigate();
     return(
       // The Router is below. Everything within the <Router></Router>
@@ -18,27 +16,9 @@ function RouterComponent() {
       // will not change (see example below).
       
       <div>
+          {/* The below 2 are Button implementations... */}
           <button onClick={() => {navigate("/")}}>Home</button>
           <button onClick={() => {navigate("/about")}}>About</button>
-          <nav>
-            
-            <Link to="/"> Home </Link> 
-            <a>|</a>
-            <Link to="/about"> About </Link> 
-            <a>|</a><br></br>
-            <Link to="/profile"> Profile </Link><a> -- requires "/username"</a>
-          </nav>
-          <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/about" element={<About/>}/>
-            {/* This route accepts an additional parameter
-            `username` */}
-            <Route path="/profile/:username" element={<Profile/>}/>
-            {/* The last route should be the error page. */}
-            <Route path="*" element={<ErrorPage/>}/>
-          </Routes>
-        {/* Things outside of the <Router> tags won't change... */}
-        <div>This part doesn't change.</div>
       </div>
     );
 }
