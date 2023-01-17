d4a785ac204a85e3ca5284ac9c28eb5a7b91c860
tristanbagnulo <tristan.bagnulo1@gmail.com>
2023-01-17 11:32:44 +1100

    Added notes to Lesson 2.

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -4,9 +4,16 @@ const Counter = () => {
 
     const [count, setCount] = useState(0);
 
-    // const handleIncrement = () => {
-    //     setCount(count + 1);
-    // }
+    /* This lesson is about statefulness and the
+    lifecycle of React components. Actually, the next
+    lesson is more about that. This one is a very basic
+    introdcution to that concept. Here we introduce
+    the `useState()` hook (seen above). There are two
+    variables assigned to it; 1. the value & 2. the updater.
+    This basically allows you to call the setCount method
+    which updates the count value which then causes the
+    Counter object to refresh. Here, this change occurs
+    when teh button to increase or decrease is clicked. */
     return (
         <React.Fragment>
             <p>Count is: {count}</p>
